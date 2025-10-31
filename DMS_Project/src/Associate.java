5fb1c2068be56a742aa968ac1b4ab4656dfb89b2
LuisDuarteRepo <lduarte4@mail.valenciacollege.edu>
2025-10-31 10:42:18 -0400

    Delete DMS_Project directory
    This directory was interfering with the code so changes were not pushed effectively.

--- DMS_Project/src/Associate.java
+++
@@ -1,6 +0,0 @@
-public interface Associate {
-    //When this interface is implemented by another class, that class will define what happens in work()
-
-    void work();
-
-}
--- DMS_Project/src/Manager.java
+++
@@ -1,5 +0,0 @@
-public interface Manager {
-    //When this interface is implemented by another class, that class will define what happens in work()
-
-    void manage();
-}
